51e2c8eb23e978d91994104fb5ee5b9f79b0abb8
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-02 11:27:58 +0700

    03. Get List as JSON String

--- src/main/java/com/iffat/springboot/resttemplate/client/BeerClientImpl.java
+++ src/main/java/com/iffat/springboot/resttemplate/client/BeerClientImpl.java
@@ -1,11 +1,30 @@
 package com.iffat.springboot.resttemplate.client;
 
 import com.iffat.springboot.resttemplate.model.BeerDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@RequiredArgsConstructor
+@Service
 public class BeerClientImpl implements BeerClient {
+
+    private final RestTemplateBuilder restTemplateBuilder;
+
     @Override
     public Page<BeerDTO> listBeers() {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+
+        ResponseEntity<String> stringResponse = restTemplate.getForEntity(
+                "http://localhost:8080/api/v1/beer",
+                String.class
+        );
+
+        System.out.println(stringResponse.getBody());
+
         return null;
     }
 }
